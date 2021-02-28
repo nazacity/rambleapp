ff678c5e32f6436a352246f683c69670db594304
nazacity <warodom_tua@hotmail.com>
2021-02-28 09:57:09 +0700

    updated

--- src/components/social/social/SocialCommentModal.js
+++ src/components/social/social/SocialCommentModal.js
@@ -8,6 +8,7 @@ import {
   Text,
   SafeAreaView,
   TouchableOpacity,
+  KeyboardAvoidingView,
 } from 'react-native';
 import {Input, Avatar} from 'react-native-elements';
 import Modal from 'react-native-modal';
@@ -88,33 +89,35 @@ const SocialCommentModal = ({
         ],
       );
       return;
+    } else {
+      dispatch(setLoading(true));
+      const convertImage = async () => {
+        return Promise.all(
+          result.map(async (item) => {
+            const resizeItem = await ImageResizer.createResizedImage(
+              Platform.OS === 'ios' ? item.sourceURL : item.path,
+              600,
+              600,
+              'JPEG',
+              100,
+              0,
+              undefined,
+              false,
+              {mode: 'contain', onlyScaleDown: false},
+            );
+
+            return {
+              uri: resizeItem.uri,
+              type: item.mime,
+              name: resizeItem.name,
+            };
+          }),
+        );
+      };
+      const data = await convertImage();
+      setImages([...images, ...data]);
+      dispatch(setLoading(false));
     }
-
-    const convertImage = async () => {
-      return Promise.all(
-        result.map(async (item) => {
-          const resizeItem = await ImageResizer.createResizedImage(
-            Platform.OS === 'ios' ? item.sourceURL : item.path,
-            600,
-            600,
-            'JPEG',
-            100,
-            0,
-            undefined,
-            false,
-            {mode: 'contain', onlyScaleDown: false},
-          );
-
-          return {
-            uri: resizeItem.uri,
-            type: item.mime,
-            name: resizeItem.name,
-          };
-        }),
-      );
-    };
-    const data = await convertImage();
-    setImages([...images, ...data]);
   };
 
   const handleReset = () => {
@@ -198,220 +201,223 @@ const SocialCommentModal = ({
       style={{margin: 0, justifyContent: 'flex-end'}}
       onBackdropPress={handleReset}
       onBackButtonPress={handleReset}>
-      <View
-        style={{
-          backgroundColor: COLORS.white,
-          padding: 20,
-          borderTopLeftRadius: 20,
-          borderTopRightRadius: 20,
-        }}>
-        <ModalCloseButton onPress={handleReset} />
-        <ScrollView showsVerticalScrollIndicator={false}>
-          <View style={{flexDirection: 'row', alignItems: 'center'}}>
-            <Avatar rounded source={{uri: user.user_picture_url}} size={40} />
-            <View style={{marginLeft: 10}}>
-              <Text style={[FONTS.h2]}>{user.display_name}</Text>
-            </View>
-          </View>
-
-          <View style={{marginBottom: 10}}>
-            <Input
-              placeholder={t('community.socialcomment.post')}
-              inputContainerStyle={{
-                borderBottomWidth: 0,
-              }}
-              onChangeText={(text) => {
-                if (text.length > 1500) {
-                  Alert.alert(t('community.comment.socialcomment'), '', [
-                    {
-                      text: t('community.comment.okay'),
-                      onPress: () => console.log('okay'),
-                    },
-                  ]);
-                } else {
-                  setValue(text);
-                }
-              }}
-              value={value}
-              multiline
-              numberOfLines={5}
-              //   containerStyle={{
-              //     backgroundColor: COLORS.lightGrey,
-
-              //   }}
-              style={[FONTS.body3, {textAlignVertical: 'top'}]}
-            />
-
-            <View>
-              <Text
-                style={[FONTS.body4, {textAlign: 'right', marginBottom: 10}]}>
-                {value.length} / 1500
-              </Text>
-              <TouchableOpacity
-                disabled={loading || value.length === 0 ? true : false}
-                style={{
-                  backgroundColor:
-                    loading || value.length === 0
-                      ? COLORS.inputPlaceholderColor
-                      : COLORS.primary,
-                  paddingVertical: 5,
-                  borderRadius: 2,
-                  marginBottom: 10,
-                  marginHorizontal: 20,
-                }}
-                onPress={onSubmit}>
-                <Text
-                  style={[
-                    FONTS.body4,
-                    {color: COLORS.white, textAlign: 'center'},
-                  ]}>
-                  {t('community.socialcomment.post')}
-                </Text>
-              </TouchableOpacity>
-            </View>
-
-            <View
-              style={{
-                borderBottomWidth: 0.5,
-                width: '100%',
-                alignSelf: 'center',
-                borderColor: 'rgba(0,0,0,0.3)',
-                marginBottom: 10,
-              }}
-            />
-            <View
-              style={{
-                flexDirection: 'row',
-                alignItems: 'center',
-                justifyContent: 'space-between',
-              }}>
-              <View style={{flexDirection: 'row'}}>
-                <TouchableOpacity
-                  activeOpacity={0.8}
-                  style={{width: 30, height: 30, marginRight: 5}}
-                  onPress={() => {
-                    takePhotoFromCamera();
-                  }}>
-                  <Ionicons
-                    name="camera"
-                    size={24}
-                    color={COLORS.inputPlaceholderColor}
-                  />
-                </TouchableOpacity>
-                <TouchableOpacity
-                  activeOpacity={0.8}
-                  style={{width: 30, height: 30}}
-                  onPress={() => {
-                    choosePhotoFromLibrary();
-                  }}>
-                  <Ionicons
-                    name="image-outline"
-                    size={24}
-                    color={COLORS.inputPlaceholderColor}
-                  />
-                </TouchableOpacity>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+        <View
+          style={{
+            backgroundColor: COLORS.white,
+            padding: 20,
+            borderTopLeftRadius: 20,
+            borderTopRightRadius: 20,
+          }}>
+          <ModalCloseButton onPress={handleReset} />
+          <ScrollView showsVerticalScrollIndicator={false}>
+            <View style={{flexDirection: 'row', alignItems: 'center'}}>
+              <Avatar rounded source={{uri: user.user_picture_url}} size={40} />
+              <View style={{marginLeft: 10}}>
+                <Text style={[FONTS.h2]}>{user.display_name}</Text>
               </View>
-              <Text
-                style={[FONTS.body4, {textAlign: 'right', marginBottom: 10}]}>
-                {images.length} / 3
-              </Text>
             </View>
-          </View>
 
-          {images.length === 1 && (
-            <View style={{marginBottom: 5}}>
-              <TouchableOpacity
-                activeOpacity={0.4}
-                onPress={() => {
-                  if (images.length === 1) {
-                    setImages([]);
+            <View style={{marginBottom: 10}}>
+              <Input
+                placeholder={t('community.socialcomment.post')}
+                inputContainerStyle={{
+                  borderBottomWidth: 0,
+                }}
+                onChangeText={(text) => {
+                  if (text.length > 1500) {
+                    Alert.alert(t('community.comment.socialcomment'), '', [
+                      {
+                        text: t('community.comment.okay'),
+                        onPress: () => console.log('okay'),
+                      },
+                    ]);
                   } else {
-                    const newImages = images.filter(
-                      (item) => item.uri !== pic.uri,
-                    );
-
-                    setImages([...newImages]);
+                    setValue(text);
                   }
                 }}
-                style={{
-                  position: 'absolute',
-                  borderRadius: 10,
-                  top: 5,
-                  right: 5,
-                  zIndex: 200,
-                }}>
-                <Ionicons
-                  onPress={() => {
-                    setImages([]);
+                value={value}
+                multiline
+                numberOfLines={5}
+                //   containerStyle={{
+                //     backgroundColor: COLORS.lightGrey,
+
+                //   }}
+                style={[FONTS.body3, {textAlignVertical: 'top'}]}
+              />
+
+              <View>
+                <Text
+                  style={[FONTS.body4, {textAlign: 'right', marginBottom: 10}]}>
+                  {value.length} / 1500
+                </Text>
+                <TouchableOpacity
+                  disabled={loading || value.length === 0 ? true : false}
+                  style={{
+                    backgroundColor:
+                      loading || value.length === 0
+                        ? COLORS.inputPlaceholderColor
+                        : COLORS.primary,
+                    paddingVertical: 5,
+                    borderRadius: 2,
+                    marginBottom: 10,
+                    marginHorizontal: 20,
                   }}
-                  size={20}
-                  name="close"
-                  color="#fff"
-                />
-              </TouchableOpacity>
-              <Image
-                source={{
-                  uri: images[0].uri,
-                }}
+                  onPress={onSubmit}>
+                  <Text
+                    style={[
+                      FONTS.body4,
+                      {color: COLORS.white, textAlign: 'center'},
+                    ]}>
+                    {t('community.socialcomment.post')}
+                  </Text>
+                </TouchableOpacity>
+              </View>
+
+              <View
                 style={{
-                  width: SIZES.width - 40,
-                  height: SIZES.width - 40,
-                  borderRadius: 5,
+                  borderBottomWidth: 0.5,
+                  width: '100%',
+                  alignSelf: 'center',
+                  borderColor: 'rgba(0,0,0,0.3)',
+                  marginBottom: 10,
                 }}
               />
+              <View
+                style={{
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                  justifyContent: 'space-between',
+                }}>
+                <View style={{flexDirection: 'row'}}>
+                  <TouchableOpacity
+                    activeOpacity={0.8}
+                    style={{width: 30, height: 30, marginRight: 5}}
+                    onPress={() => {
+                      takePhotoFromCamera();
+                    }}>
+                    <Ionicons
+                      name="camera"
+                      size={24}
+                      color={COLORS.inputPlaceholderColor}
+                    />
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                    activeOpacity={0.8}
+                    style={{width: 30, height: 30}}
+                    onPress={() => {
+                      choosePhotoFromLibrary();
+                    }}>
+                    <Ionicons
+                      name="image-outline"
+                      size={24}
+                      color={COLORS.inputPlaceholderColor}
+                    />
+                  </TouchableOpacity>
+                </View>
+                <Text
+                  style={[FONTS.body4, {textAlign: 'right', marginBottom: 10}]}>
+                  {images.length} / 3
+                </Text>
+              </View>
             </View>
-          )}
-          {images.length > 1 && (
-            <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-              <View style={{flexDirection: 'row', justifyContent: 'center'}}>
-                {images.map((pic, index) => {
-                  return (
-                    <View
-                      key={index}
-                      style={{
-                        height: 150,
-                        width: 150,
-                        borderRadius: 5,
-                        marginRight: index === images.length - 1 ? 0 : 10,
-                      }}>
-                      <TouchableOpacity
-                        activeOpacity={0.8}
-                        onPress={() => {
-                          if (images.length === 1) {
-                            setImages([]);
-                          } else {
-                            const newImages = images.filter(
-                              (item) => item.uri !== pic.uri,
-                            );
 
-                            setImages([...newImages]);
-                          }
-                        }}
-                        style={{
-                          position: 'absolute',
-                          borderRadius: 10,
-                          top: 5,
-                          right: 5,
-                          zIndex: 200,
-                        }}>
-                        <Ionicons size={20} name="close" color="#fff" />
-                      </TouchableOpacity>
-                      <Image
-                        source={{uri: pic.uri}}
+            {images.length === 1 && (
+              <View style={{marginBottom: 5}}>
+                <TouchableOpacity
+                  activeOpacity={0.4}
+                  onPress={() => {
+                    if (images.length === 1) {
+                      setImages([]);
+                    } else {
+                      const newImages = images.filter(
+                        (item) => item.uri !== pic.uri,
+                      );
+
+                      setImages([...newImages]);
+                    }
+                  }}
+                  style={{
+                    position: 'absolute',
+                    borderRadius: 10,
+                    top: 5,
+                    right: 5,
+                    zIndex: 200,
+                  }}>
+                  <Ionicons
+                    onPress={() => {
+                      setImages([]);
+                    }}
+                    size={20}
+                    name="close"
+                    color="#fff"
+                  />
+                </TouchableOpacity>
+                <Image
+                  source={{
+                    uri: images[0].uri,
+                  }}
+                  style={{
+                    width: SIZES.width - 40,
+                    height: SIZES.width - 40,
+                    borderRadius: 5,
+                  }}
+                />
+              </View>
+            )}
+            {images.length > 1 && (
+              <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+                <View style={{flexDirection: 'row', justifyContent: 'center'}}>
+                  {images.map((pic, index) => {
+                    return (
+                      <View
+                        key={index}
                         style={{
                           height: 150,
                           width: 150,
                           borderRadius: 5,
-                        }}
-                      />
-                    </View>
-                  );
-                })}
-              </View>
-            </ScrollView>
-          )}
-        </ScrollView>
-      </View>
+                          marginRight: index === images.length - 1 ? 0 : 10,
+                        }}>
+                        <TouchableOpacity
+                          activeOpacity={0.8}
+                          onPress={() => {
+                            if (images.length === 1) {
+                              setImages([]);
+                            } else {
+                              const newImages = images.filter(
+                                (item) => item.uri !== pic.uri,
+                              );
+
+                              setImages([...newImages]);
+                            }
+                          }}
+                          style={{
+                            position: 'absolute',
+                            borderRadius: 10,
+                            top: 5,
+                            right: 5,
+                            zIndex: 200,
+                          }}>
+                          <Ionicons size={20} name="close" color="#fff" />
+                        </TouchableOpacity>
+                        <Image
+                          source={{uri: pic.uri}}
+                          style={{
+                            height: 150,
+                            width: 150,
+                            borderRadius: 5,
+                          }}
+                        />
+                      </View>
+                    );
+                  })}
+                </View>
+              </ScrollView>
+            )}
+          </ScrollView>
+        </View>
+      </KeyboardAvoidingView>
     </Modal>
   );
 };
