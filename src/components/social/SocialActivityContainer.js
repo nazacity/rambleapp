5ef739f4027440f799b56549c4383cc41f6b1a33
nazacity <warodom_tua@hotmail.com>
2021-03-03 15:44:10 +0700

    updated

--- src/components/social/SocialActivityContainer.js
+++ src/components/social/SocialActivityContainer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   View,
   Text,
@@ -9,15 +9,33 @@ import {
 } from 'react-native';
 import {useSelector} from 'react-redux';
 import {useNavigation} from '@react-navigation/native';
-import {COLORS, SHADOW} from '../../constants';
+import {COLORS, FONTS, SHADOW} from '../../constants';
 import LocalizationContext from '../../screens/LocalizationContext';
 
 const SocialActivityContainer = () => {
   const user_activities = useSelector((state) => state.user.user_activities);
-  const data = user_activities.slice(0, 5);
+  // const data = user_activities.slice(0, 5);
   //   const data = [...user_activities, ...user_activities, ...user_activities];
+  const [data, setData] = useState([]);
   const {t} = React.useContext(LocalizationContext);
   const navigation = useNavigation();
+
+  const checkUserActivities = () => {
+    const newData = user_activities
+      .filter(
+        (item) => item.state !== 'waiting_payment' && item.state !== 'cancel',
+      )
+      .slice(0, 5);
+    setData(newData);
+  };
+
+  useEffect(() => {
+    const unsubscribe = navigation.addListener('focus', () => {
+      checkUserActivities();
+    });
+
+    return unsubscribe;
+  }, []);
 
   const SocialCard = ({item, index}) => {
     return (
@@ -62,6 +80,15 @@ const SocialActivityContainer = () => {
       </View>
     );
   };
+
+  if (data.length === 0) {
+    return (
+      <View
+        style={{alignItems: 'center', height: 140, justifyContent: 'center'}}>
+        <Text style={[FONTS.body2]}>{t('community.noactivity')}</Text>
+      </View>
+    );
+  }
 
   return (
     <FlatList
