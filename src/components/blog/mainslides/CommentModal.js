4ff40b07c78a41e816a97d63d4e226e01492cafa
nazacity <warodom_tua@hotmail.com>
2021-03-04 17:21:43 +0700

    updated

--- src/components/blog/mainslides/CommentModal.js
+++ src/components/blog/mainslides/CommentModal.js
@@ -7,6 +7,8 @@ import LocalizationContext from '../../../screens/LocalizationContext';
 import CommentTab from './CommentTab2';
 import ModalCloseButton from '../../layout/ModalCloseButton';
 import CommentCard from './CommentCard';
+import {KeyboardAvoidingView} from 'react-native';
+import {Fragment} from 'react';
 
 const CommentModal = ({open, handleClose, data, setData}) => {
   const {t} = React.useContext(LocalizationContext);
@@ -17,30 +19,62 @@ const CommentModal = ({open, handleClose, data, setData}) => {
       style={{margin: 0, justifyContent: 'flex-end'}}
       onBackdropPress={handleClose}
       onBackButtonPress={handleClose}>
-      <View
-        style={{
-          backgroundColor: COLORS.white,
-          borderTopLeftRadius: 20,
-          borderTopRightRadius: 20,
-        }}>
-        <ModalCloseButton onPress={handleClose} />
-        <FlatList
-          showsVerticalScrollIndicator={false}
-          data={data.blog_comments}
-          style={{marginTop: 40}}
-          contentContainerStyle={{padding: 10}}
-          keyExtractor={(item) => item._id}
-          renderItem={({item, index}) => {
-            return <CommentCard item={item} index={index} />;
-          }}
-          ListFooterComponent={<View style={{margin: 60}} />}
-        />
-      </View>
-      <CommentTab
-        bottom={Platform.OS === 'ios' ? 40 : 20}
-        id={data._id}
-        setData={setData}
-      />
+      {Platform.OS === 'ios' ? (
+        <KeyboardAvoidingView
+          behavior={Platform.OS === 'ios' ? 'position' : 'height'}>
+          <View
+            style={{
+              backgroundColor: COLORS.white,
+              borderTopLeftRadius: 20,
+              borderTopRightRadius: 20,
+            }}>
+            <ModalCloseButton onPress={handleClose} />
+            <FlatList
+              showsVerticalScrollIndicator={false}
+              data={data.blog_comments}
+              style={{marginTop: 40}}
+              contentContainerStyle={{padding: 10}}
+              keyExtractor={(item) => item._id}
+              renderItem={({item, index}) => {
+                return <CommentCard item={item} index={index} />;
+              }}
+              ListFooterComponent={<View style={{margin: 60}} />}
+            />
+          </View>
+          <CommentTab
+            bottom={Platform.OS === 'ios' ? 40 : 20}
+            id={data._id}
+            setData={setData}
+          />
+        </KeyboardAvoidingView>
+      ) : (
+        <Fragment>
+          <View
+            style={{
+              backgroundColor: COLORS.white,
+              borderTopLeftRadius: 20,
+              borderTopRightRadius: 20,
+            }}>
+            <ModalCloseButton onPress={handleClose} />
+            <FlatList
+              showsVerticalScrollIndicator={false}
+              data={data.blog_comments}
+              style={{marginTop: 40}}
+              contentContainerStyle={{padding: 10}}
+              keyExtractor={(item) => item._id}
+              renderItem={({item, index}) => {
+                return <CommentCard item={item} index={index} />;
+              }}
+              ListFooterComponent={<View style={{margin: 60}} />}
+            />
+          </View>
+          <CommentTab
+            bottom={Platform.OS === 'ios' ? 40 : 20}
+            id={data._id}
+            setData={setData}
+          />
+        </Fragment>
+      )}
     </Modal>
   );
 };
--- src/components/blog/mainslides/CommentTab2.js
+++ src/components/blog/mainslides/CommentTab2.js
@@ -36,82 +36,76 @@ const CommentTab = ({setOpen, bottom, id, setData}) => {
   };
 
   return (
-    <KeyboardAvoidingView
-      behavior={Platform.OS === 'ios' ? 'position' : 'height'}
-      style={{
-        position: 'absolute',
-        left: 0,
-        right: 0,
-        bottom: 0,
-        zIndex: 100,
-      }}>
-      <View
-        style={[
-          {
-            backgroundColor: COLORS.white,
-            padding: 10,
-            paddingHorizontal: 20,
-            flexDirection: 'row',
-            alignItems: 'center',
-          },
-        ]}>
-        <View style={{flex: 1}}>
-          <Input
-            placeholder={t('community.comment.commentblog')}
-            inputContainerStyle={{
-              borderBottomWidth: 0,
-              top: Platform.OS === 'ios' ? 5 : 0,
-            }}
-            onChangeText={(text) => {
-              if (text.length > 500) {
-                Alert.alert(t('community.comment.textmorethan'), '', [
-                  {
-                    text: t('community.comment.okay'),
-                    onPress: () => console.log('okay'),
-                  },
-                ]);
-              } else {
-                setValue(text);
-              }
-            }}
-            value={value}
-            multiline
-            containerStyle={{
-              backgroundColor: COLORS.lightGrey,
-              borderRadius: 5,
-              height: value.length > 30 ? undefined : 50,
-              borderColor: COLORS.black,
-              borderWidth: 0.5,
-              borderRadius: 5,
-            }}
-            style={[FONTS.body3]}
-          />
-          <Text style={[FONTS.body4, {textAlign: 'right'}]}>
-            {value.length} / 500
-          </Text>
-        </View>
-        <TouchableOpacity
-          disabled={!value}
-          activeOpacity={0.8}
-          style={{width: 30, height: 30, marginLeft: 10}}
-          onPress={handleComment}>
-          <MaterialIcons
-            name="add-circle-outline"
-            size={24}
-            color={!value ? COLORS.inputPlaceholderColor : COLORS.primary}
-          />
-        </TouchableOpacity>
-        {setOpen && (
-          <TouchableOpacity
-            activeOpacity={0.8}
-            style={{width: 30, height: 30}}
-            onPress={() => {
-              setOpen(true);
-            }}>
-            <MaterialIcons name="more-vert" size={24} color={COLORS.primary} />
-          </TouchableOpacity>
-        )}
+    <View
+      style={[
+        {
+          backgroundColor: COLORS.white,
+          padding: 10,
+          paddingHorizontal: 20,
+          flexDirection: 'row',
+          alignItems: 'center',
+          position: 'absolute',
+          left: 0,
+          right: 0,
+          bottom: 0,
+        },
+      ]}>
+      <View style={{flex: 1}}>
+        <Input
+          placeholder={t('community.comment.commentblog')}
+          inputContainerStyle={{
+            borderBottomWidth: 0,
+            top: Platform.OS === 'ios' ? 5 : 0,
+          }}
+          onChangeText={(text) => {
+            if (text.length > 500) {
+              Alert.alert(t('community.comment.textmorethan'), '', [
+                {
+                  text: t('community.comment.okay'),
+                  onPress: () => console.log('okay'),
+                },
+              ]);
+            } else {
+              setValue(text);
+            }
+          }}
+          value={value}
+          multiline
+          containerStyle={{
+            backgroundColor: COLORS.lightGrey,
+            borderRadius: 5,
+            height: value.length > 30 ? undefined : 50,
+            borderColor: COLORS.black,
+            borderWidth: 0.5,
+            borderRadius: 5,
+          }}
+          style={[FONTS.body3]}
+        />
+        <Text style={[FONTS.body4, {textAlign: 'right'}]}>
+          {value.length} / 500
+        </Text>
       </View>
+      <TouchableOpacity
+        disabled={!value}
+        activeOpacity={0.8}
+        style={{width: 30, height: 30, marginLeft: 10}}
+        onPress={handleComment}>
+        <MaterialIcons
+          name="add-circle-outline"
+          size={24}
+          color={!value ? COLORS.inputPlaceholderColor : COLORS.primary}
+        />
+      </TouchableOpacity>
+      {setOpen && (
+        <TouchableOpacity
+          activeOpacity={0.8}
+          style={{width: 30, height: 30}}
+          onPress={() => {
+            setOpen(true);
+          }}>
+          <MaterialIcons name="more-vert" size={24} color={COLORS.primary} />
+        </TouchableOpacity>
+      )}
       <Snackbar
         visible={error}
         onDismiss={() => {
@@ -123,7 +117,7 @@ const CommentTab = ({setOpen, bottom, id, setData}) => {
         duration={1500}>
         {message}
       </Snackbar>
-    </KeyboardAvoidingView>
+    </View>
   );
 };
 
