eacbf9d368410f7282334ea8b4d3a745fb56a1bb
nazacity <warodom_tua@hotmail.com>
2021-03-29 11:03:12 +0700

    updated

--- src/components/activity/Course.js
+++ src/components/activity/Course.js
@@ -4,6 +4,7 @@ import {FONTS, COLORS, SHADOW} from '../../constants';
 import ImageModal from 'react-native-image-modal';
 import LocalizationContext from '../../screens/LocalizationContext';
 import TitleHeader from '../layout/TitleHeader';
+import LinearGradient from 'react-native-linear-gradient';
 
 const Course = ({course}) => {
   const {t} = React.useContext(LocalizationContext);
@@ -25,15 +26,16 @@ const Course = ({course}) => {
               marginBottom: 10,
               backgroundColor: COLORS.white,
               borderRadius: 5,
+              width: 300,
+              height: 200,
             },
             SHADOW.default,
           ]}>
           <View
             style={{
-              width: 100,
-              height: 100,
-              borderBottomLeftRadius: 5,
-              borderTopLeftRadius: 5,
+              width: 300,
+              height: 200,
+              borderRadius: 5,
               overflow: 'hidden',
             }}>
             <ImageModal
@@ -42,21 +44,37 @@ const Course = ({course}) => {
               imageBackgroundColor={COLORS.background}
               overlayBackgroundColor={COLORS.darkOpacityBlack}
               style={{
-                width: 100,
-                height: 100,
-                borderBottomLeftRadius: 5,
-                borderTopLeftRadius: 5,
+                width: 300,
+                height: 200,
+                borderRadius: 5,
               }}
               source={{
                 uri: course.course_picture_url,
               }}
             />
+            <LinearGradient
+              colors={['rgba(0,0,0,0.0)', 'rgba(0,0,0,0.3)', 'rgba(0,0,0,1)']}
+              start={{x: 0, y: 1}}
+              end={{x: 1, y: 1}}
+              useAngle
+              angle={180}
+              style={{
+                flex: 1,
+                left: 0,
+                top: 0,
+                width: 300,
+                height: 200,
+                position: 'absolute',
+              }}
+            />
           </View>
-          <View>
-            <Text style={[FONTS.body3, {marginLeft: 20}]}>{course.title}</Text>
-            <Text style={[FONTS.body3, {marginLeft: 20}]}>
-              {t('activity.fee')} {course.price} {t('activity.bath')}
+          <View style={{position: 'absolute', bottom: 20, left: 20}}>
+            <Text style={[FONTS.body3, {color: COLORS.white, lineHeight: 18}]}>
+              {course.title}
             </Text>
+            {/* <Text style={[FONTS.body3, {color: COLORS.white, lineHeight: 18}]}>
+              {t('activity.fee')} {course.price} {t('activity.bath')}
+            </Text> */}
           </View>
         </TouchableOpacity>
       </View>
